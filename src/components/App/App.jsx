da66395c4bce2b2ca6f93bf70e6760c712b9d8ff
Peder Goodman <peder.goodman@gmail.com>
2023-07-13 11:53:17 -0500

    added git request for pizza objects

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,58 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
+
+import { useDispatch } from 'react-redux';
+
 function App() {
+
+  // GET for pizza objects
+  const fetchPizzaList = () => {
+
+    axios.get('/api/pizza').then((response) => {
+      console.log(response.data);
+
+      dispatch({
+        type: 'SET_PIZZA_LIST',
+        payload: response.data
+      })
+
+    }).catch((err) => {
+      console.log('Error GETing pizza list', err);
+    });
+
+  }; // end fetchPizzaList
+
+
+  // load pizza list 
+  useEffect(() => {
+    fetchPizzaList();
+  }, [])
+
+
+
+
+
+
+
+
 
   return (
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Prime Pizza</h1>
       </header>
-  
+
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
-  
+
     </div>
   );
 }
 
 export default App;
+
+
+
+
