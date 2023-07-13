2f3b43f8df313b5eecb091c9998d2be7f442b587
Peder Goodman <peder.goodman@gmail.com>
2023-07-13 14:04:01 -0500

    started post request

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,16 +7,18 @@ import { useDispatch } from 'react-redux';
 
 function App() {
 
+  // const dispatch = useDispatch();
+
   // GET for pizza objects
   const fetchPizzaList = () => {
 
     axios.get('/api/pizza').then((response) => {
       console.log(response.data);
 
-      dispatch({
-        type: 'SET_PIZZA_LIST',
-        payload: response.data
-      })
+      // dispatch({
+      //   type: 'SAVE_PIZZAS',
+      //   payload: response.data
+      // })
 
     }).catch((err) => {
       console.log('Error GETing pizza list', err);
@@ -28,8 +30,49 @@ function App() {
   // load pizza list 
   useEffect(() => {
     fetchPizzaList();
+    fetchCompletedOrders();
   }, [])
 
+
+    // GET for pizza objects
+    const fetchCompletedOrders = () => {
+
+      axios.get('/api/order').then((response) => {
+        console.log('Orders list is:', response.data);
+  
+        // dispatch({
+        //   type: 'SAVE_ORDERS',
+        //   payload: response.data
+        // })
+  
+      }).catch((err) => {
+        console.log('Error GETing completed order list', err);
+      });
+  
+    }; // end fetchCompletedOrders
+
+
+    // POST object
+    const sendOrderToServer = () => {
+      
+      axios.post('/api/order', )
+
+      // {
+      //   "customer_name": "Donatello",
+      //   "street_address": "20 W 34th St",
+      //   "city": "New York",
+      //   "zip": "10001",
+      //   "total": "27.98",
+      //   "type": "Pickup",
+      //   "pizzas": [{
+      //     "id": "1",
+      //     "quantity": "1"
+      //   },{
+      //     "id": "2",
+      //     "quantity": "1"
+      //   }]
+      // }
+    }
 
 
 
